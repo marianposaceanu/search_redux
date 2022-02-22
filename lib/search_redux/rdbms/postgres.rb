dd709844990273e7e4a288f00d5dd5b89167be70
Marian Posaceanu <contact@marianposaceanu.com>
2022-02-22 22:55:09 +0200

    feat!:start updating logic for new major version

--- lib/search_redux/rdbms/postgres.rb
+++ lib/search_redux/rdbms/postgres.rb
@@ -4,18 +4,15 @@ module SearchRedux
       attr_accessor :sanitized_query
 
       def compatible_search(options)
-        query           = options[:query]
+        query        = options[:query]
+        rank_query   = rank_sql(options[:rank], query)
+        search_query = search_sql(options[:columns])
 
-        rank_query      = rank_sql options[:rank], query
-        search_query    = search_sql options[:columns]
-
-        ->(obj) { obj.where(search_query, q: query).order(rank_query) }
+        ->(obj) { obj.where(search_query, q: Arel.sql(query)).order(Arel.sql(rank_query)) }
       end
 
       def rank_sql(rank_column, query)
-        sanitized_query = ActionController::Base.helpers.sanitize(query)
-
-        "ts_rank(to_tsvector(#{rank_column}), plainto_tsquery('#{sanitized_query}')) desc"
+        "ts_rank(to_tsvector(#{rank_column}), plainto_tsquery('#{query}')) desc"
       end
 
       def search_sql(columns)
