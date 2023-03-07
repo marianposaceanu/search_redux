953009c847154170a10a946e54316f93b0f37905
Marian Posaceanu <contact@marianposaceanu.com>
2023-03-07 09:38:47 +0200

    feat(fuzzy) add fuzzy full text search

--- lib/search_redux/rdbms/postgres.rb
+++ lib/search_redux/rdbms/postgres.rb
@@ -4,20 +4,24 @@ module SearchRedux
       attr_accessor :sanitized_query
 
       def compatible_search(options)
-        query        = options[:query]
+        query        = to_ts_query(options[:query])
         rank_query   = rank_sql(options[:rank], query)
         search_query = search_sql(options[:columns])
 
         ->(obj) { obj.where(Arel.sql(search_query), q: Arel.sql(query)).order(Arel.sql(rank_query)) }
       end
 
+      def to_ts_query(query)
+        query.split.map { |ts| "#{ts}:*" }.join(" & ")
+      end
+
       def rank_sql(rank_column, query)
-        "ts_rank(to_tsvector(#{rank_column}), plainto_tsquery('#{query}')) desc"
+        "ts_rank(to_tsvector(#{rank_column}), to_tsquery('#{query}')) desc"
       end
 
       def search_sql(columns)
         template = ->(column_name) {
-          return "to_tsvector('english', #{column_name}) @@ plainto_tsquery(:q)"
+          return "to_tsvector('english', #{column_name}) @@ to_tsquery(:q)"
         }
 
         columns.map { |c| template.call(c) }.join(' OR ')
